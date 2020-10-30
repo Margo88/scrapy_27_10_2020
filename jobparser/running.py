9eacddb94c4f03abeb9afc557fc769651d653d9a
Margo88 <sokolovamk88@gmail.com>
2020-10-30 11:54:49 +0300

    Homework6

---
+++ jobparser/running.py
@@ -0,0 +1,30 @@
+"""I вариант
+1) Доработать паука в имеющемся проекте, чтобы он формировал item по структуре:
+*Наименование вакансии
+*Зарплата от
+*Зарплата до
+*Ссылку на саму вакансию
+*Сайт откуда собрана вакансия
+И складывал все записи в БД(любую)
+2) Создать в имеющемся проекте второго
+паука по сбору вакансий с сайта superjob.
+Паук должен формировать item'ы по аналогичной
+структуре и складывать данные также в БД"""
+
+from scrapy.crawler import CrawlerProcess
+from scrapy.settings import  Settings
+
+from jobparser import settings
+from jobparser.spiders.hhru import HhruSpider
+from jobparser.spiders.sjru import SjruSpider
+
+
+if __name__ == '__main__':
+    crawler_settings = Settings()
+    crawler_settings.setmodule(settings)
+
+    process = CrawlerProcess(settings=crawler_settings)
+    process.crawl(HhruSpider)
+    process.crawl(SjruSpider)
+
+    process.start()
